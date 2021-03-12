0a7541c531b62f9bfa08ad885f909912b9c5fab1
Mick Lawitzke <info@lawitzke.de>
2021-03-12 20:21:33 +0100

    Removed dependency

--- lib/index.ts
+++ lib/index.ts
@@ -1,17 +1,17 @@
 import { Client } from "nats";
 import * as _debug from "debug";
 import {
-  NatsAdapterOptionsDto,
-  SUBJECT_KEY,
+    NatsAdapterOptionsDto,
+    SUBJECT_KEY,
 } from "@mickl/socket.io-nats-adapter";
 import { BroadcastFlags, Room, SocketId } from "socket.io-adapter";
-import { Packet, PacketType } from "socket.io-parser";
+import { Packet } from "socket.io-parser";
 
 const debug = _debug("socket.io-nats-emitter");
 
 export interface NatsEmitterOptions {
-  key?: string;
-  nsp?: string;
+    key?: string;
+    nsp?: string;
 }
 
 /**
@@ -20,66 +20,66 @@ export interface NatsEmitterOptions {
  * TODO: Add return type https://github.com/socketio/socket.io/issues/3796
  */
 export const createEmitter = (client: Client, opts?: NatsEmitterOptions) => {
-  return new NatsEmitter(client, opts?.nsp, opts?.key);
+    return new NatsEmitter(client, opts?.nsp, opts?.key);
 };
 
 export class NatsEmitter {
-  private subject: string;
-  private uid = "socket.io-nats-emitter";
-  private except: SocketId[] = [];
-  private flags: BroadcastFlags = {};
-  private rooms: Room[] = [];
+    private subject: string;
+    private uid                   = "socket.io-nats-emitter";
+    private except: SocketId[]    = [];
+    private flags: BroadcastFlags = {};
+    private rooms: Room[]         = [];
 
-  constructor(
-    private client: Client,
-    private nsp = "/",
-    private key = SUBJECT_KEY
-  ) {
-    this.subject = key + "." + nsp;
-  }
+    constructor(
+        private client: Client,
+        private nsp = "/",
+        private key = SUBJECT_KEY
+    ) {
+        this.subject = key + "." + nsp;
+    }
 
-  of(nsp: string): NatsEmitter {
-    return new NatsEmitter(
-      this.client,
-      (nsp !== "/" ? "/" : "") + nsp,
-      this.subject
-    );
-  }
+    of(nsp: string): NatsEmitter {
+        return new NatsEmitter(
+            this.client,
+            (nsp !== "/" ? "/" : "") + nsp,
+            this.subject
+        );
+    }
 
-  to(room: string): this {
-    this.rooms.push(room);
-    return this;
-  }
+    to(room: string): this {
+        this.rooms.push(room);
+        return this;
+    }
 
-  /**
-   * Send the packet
-   */
-  emit(event: string, ...data: any): this {
-    const packet: Packet = {
-      type: PacketType.EVENT,
-      data: [event, ...data],
-      nsp: this.nsp,
-    };
+    /**
+     * Send the packet
+     */
+    emit(event: string, ...data: any): this {
+        const packet: Packet = {
+            type: 2,
+            data: [event, ...data],
+            nsp:  this.nsp,
+        };
 
-    const dto: NatsAdapterOptionsDto = {
-      fromUid: this.uid,
-      packet,
-      opts: {
-        rooms: Array.from(this.rooms),
-        except: Array.from(this.except),
-        flags: this.flags,
-      },
-    };
+        const dto: NatsAdapterOptionsDto = {
+            fromUid: this.uid,
+            packet,
+            opts:    {
+                rooms:  Array.from(this.rooms),
+                except: Array.from(this.except),
+                flags:  this.flags,
+            },
+        };
 
-    const msg = JSON.stringify(dto);
+        const msg = JSON.stringify(dto);
 
-    debug("Publishing message to subject '%s'", this.subject);
-    this.client.publish(this.subject, msg);
+        debug("Publishing message to subject '%s'", this.subject);
+        this.client.publish(this.subject, msg);
 
-    this.rooms = [];
-    this.except = [];
-    this.flags = {};
+        this.rooms  = [];
+        this.except = [];
+        this.flags  = {};
 
-    return this;
-  }
+        return this;
+    }
 }

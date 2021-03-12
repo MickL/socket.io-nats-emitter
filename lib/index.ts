15047d0520457af3b43e710091761920e9d6c31c
Mick Lawitzke <info@lawitzke.de>
2021-03-12 19:36:40 +0100

    Emit, emit to room(s), namespaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,82 @@
 import { Client } from "nats";
 import * as _debug from "debug";
+import { NatsAdapterOptionsDto, SUBJECT_KEY } from "socket.io-nats-adapter";
+import { BroadcastFlags, Room, SocketId } from "socket.io-adapter";
+import { Packet, PacketType } from "socket.io-parser";
 
 const debug = _debug("socket.io-nats-emitter");
 
-// TODO
+export interface NatsEmitterOptions {
+  key?: string;
+  nsp?: string;
+}
+
+/**
+ * Returns a NATS Adapter function
+ *
+ * TODO: Add return type https://github.com/socketio/socket.io/issues/3796
+ */
+export const createEmitter = (client: Client, opts?: NatsEmitterOptions) => {
+  return new NatsEmitter(client, opts?.nsp, opts?.key);
+};
+
+export class NatsEmitter {
+  private subject: string;
+  private uid = "socket.io-nats-emitter";
+  private except: SocketId[] = [];
+  private flags: BroadcastFlags = {};
+  private rooms: Room[] = [];
+
+  constructor(
+    private client: Client,
+    private nsp = "/",
+    private key = SUBJECT_KEY
+  ) {
+    this.subject = key + "." + nsp;
+  }
+
+  of(nsp: string): NatsEmitter {
+    return new NatsEmitter(
+      this.client,
+      (nsp !== "/" ? "/" : "") + nsp,
+      this.subject
+    );
+  }
+
+  to(room: string): this {
+    this.rooms.push(room);
+    return this;
+  }
+
+  /**
+   * Send the packet
+   */
+  emit(event: string, ...data: any): this {
+    const packet: Packet = {
+      type: PacketType.EVENT,
+      data: [event, ...data],
+      nsp: this.nsp,
+    };
+
+    const dto: NatsAdapterOptionsDto = {
+      fromUid: this.uid,
+      packet,
+      opts: {
+        rooms: Array.from(this.rooms),
+        except: Array.from(this.except),
+        flags: this.flags,
+      },
+    };
+
+    const msg = JSON.stringify(dto);
+
+    debug("Publishing message to subject '%s'", this.subject);
+    this.client.publish(this.subject, msg);
+
+    this.rooms = [];
+    this.except = [];
+    this.flags = {};
+
+    return this;
+  }
+}
--- test/index.ts
+++ test/index.ts
@@ -6,88 +6,133 @@ import { Socket } from "socket.io-client/build/socket";
 import { AddressInfo } from "net";
 import * as expect from "expect.js";
 import { connect } from "nats";
+import { createAdapter } from "socket.io-nats-adapter";
+import { createEmitter, NatsEmitter } from "../lib";
 
-const natsUrl                    = "localhost";
+const natsUrl = "localhost";
 const connectOptions: ClientOpts = {
-    // json: true,
-    // preserveBuffers: true,
+  // json: true,
+  // preserveBuffers: true,
 };
 
-let namespace1, namespace2, namespace3, namespace4;
-let client1, client2, client3, client4;
-let socket1, socket2, socket3, socket4;
+let namespace1, namespace2;
+let client1, client2;
+let socket1, socket2;
+let emitter: NatsEmitter;
 
 describe("socket.io-nats-emitter", () => {
-    beforeEach(async () => {
-        const natsClient = connect(natsUrl, connectOptions);
-        await init(natsClient);
-    });
-    afterEach(cleanup);
+  beforeEach(async () => {
+    const natsClient = connect(natsUrl, connectOptions);
+    await init(natsClient);
+  });
+  afterEach(cleanup);
 
-    // TODO
+  it("broadcasts", (done) => {
+    client1.on("someEvent", (a, b, c, d) => {
+      expect(a).to.eql([]);
+      expect(b).to.eql({ a: "b" });
+
+      // TODO: Buffer is not equal
+      // expect(Buffer.isBuffer(c) && c.equals(buffer)).to.be(true);
+
+      // TODO: Uint8Array is not equal
+      // expect(Buffer.isBuffer(d) && d.equals(Buffer.from(uint8Array))).to.be(true); // converted to Buffer on the client-side
+
+      done();
+    });
+
+    const buffer = Buffer.from("abcd1234", "utf8");
+    const uint8Array = Uint8Array.of(1, 2, 3, 4);
+
+    emitter.emit("someEvent", [], { a: "b" }, buffer, uint8Array);
+  });
+
+  it("broadcasts to a room", (done) => {
+    const event = "someEvent";
+    const room = "someRoom";
+
+    socket1.join(room);
+
+    client1.on(event, () => {
+      setTimeout(done, 100);
+    });
+
+    client2.on(event, () => {
+      throw new Error(
+        `Received event '${event}', but client is not in room '${room}'`
+      );
+    });
+
+    emitter.to(room).emit(event);
+  });
+
+  it("broadcasts to multiple rooms", (done) => {
+    socket1.join(["foo", "bar"]);
+
+    let called = false;
+    client1.on("broadcast", () => {
+      if (called) {
+        return done(new Error("Called more than once"));
+      }
+      called = true;
+      setTimeout(done, 100);
+    });
+
+    client2.on("broadcast", () => {
+      throw new Error("Not in room");
+    });
+
+    emitter.to("foo").to("bar").emit("broadcast");
+  });
 });
 
 async function create(
-    natsClient: Client,
-    options?: ClientOpts,
-    nsp = "/"
+  natsClient: Client,
+  options?: ClientOpts,
+  nsp = "/"
 ): Promise<{ namespace: Namespace; client: Socket; socket: any }> {
-    return new Promise((resolve) => {
-        const httpServer = createServer();
-        const ioServer   = new Server(httpServer);
+  return new Promise((resolve) => {
+    const httpServer = createServer();
+    const ioServer = new Server(httpServer);
 
-        // @ts-ignore
-        ioServer.adapter(createAdapter(natsClient, options));
+    // @ts-ignore
+    ioServer.adapter(createAdapter(natsClient, options));
 
-        httpServer.listen((a) => {
-            const port = (<AddressInfo>httpServer.address()).port;
-            const url  = "http://localhost:" + port;
+    httpServer.listen((a) => {
+      const port = (<AddressInfo>httpServer.address()).port;
+      const url = "http://localhost:" + port;
 
-            const namespace = ioServer.of(nsp);
-            const client    = io(url, {reconnection: false});
+      const namespace = ioServer.of(nsp);
+      const client = io(url, { reconnection: false });
 
-            namespace.on("connection", (socket) => {
-                resolve({namespace, client, socket});
-            });
-        });
+      namespace.on("connection", (socket) => {
+        resolve({ namespace, client, socket });
+      });
     });
+  });
 }
 
 async function init(natsClient: Client, options?: ClientOpts) {
-    const created1 = await create(natsClient, options);
-    const created2 = await create(natsClient, options);
-    const created3 = await create(natsClient, options);
-    const created4 = await create(natsClient, options);
+  const created1 = await create(natsClient, options);
+  const created2 = await create(natsClient, options);
 
-    namespace1 = created1.namespace;
-    namespace2 = created2.namespace;
-    namespace3 = created3.namespace;
-    namespace4 = created4.namespace;
+  namespace1 = created1.namespace;
+  namespace2 = created2.namespace;
 
-    client1 = created1.client;
-    client2 = created2.client;
-    client3 = created3.client;
-    client4 = created4.client;
+  client1 = created1.client;
+  client2 = created2.client;
 
-    socket1 = created1.socket;
-    socket2 = created2.socket;
-    socket3 = created3.socket;
-    socket4 = created4.socket;
+  socket1 = created1.socket;
+  socket2 = created2.socket;
+
+  emitter = createEmitter(natsClient);
 }
 
-function noop() {
-}
+function noop() {}
 
 async function cleanup() {
-    namespace1.server.close();
-    namespace2.server.close();
-    namespace3.server.close();
-    // handle 'Connection is closed' errors
-    namespace1.adapter.on("error", noop);
-    namespace2.adapter.on("error", noop);
-    namespace3.adapter.on("error", noop);
-    // TODO: Need this for Nats?
-    // namespace1.adapter.subClient.quit();
-    // namespace2.adapter.subClient.quit();
-    // namespace3.adapter.subClient.quit();
+  namespace1.server.close();
+  namespace2.server.close();
+  namespace1.adapter.on("error", noop);
+  namespace2.adapter.on("error", noop);
 }
